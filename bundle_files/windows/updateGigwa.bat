1a2ff6eb3d077f378295b2cdeb80db05579a7d93
sempere <guilhem.sempere@cirad.fr>
2019-12-04 16:38:29 +0100

    Fixed update script for Windows bundle

--- bundle_files/windows/updateGigwa.bat
+++ bundle_files/windows/updateGigwa.bat
@@ -1,8 +1,20 @@
 @echo off
 
-if not exist %1 (exit)
-if not exist %2 (exit)
-if not exist %3 (exit)
+if "%1"=="" (
+	echo You forgot to specify new gigwa path.
+	GOTO eof )
+if "%2"=="" (
+	echo You forgot to specify old gigwa path.
+	GOTO eof )
+if "%3"=="" (
+	echo You forgot to specify save path.
+	GOTO eof )
+if not exist %1 (
+	echo %1 not exist
+	GOTO eof )
+if not exist %2 (
+	echo %2 not exist
+	GOTO eof )
 
 set d=%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%
 
@@ -19,3 +31,5 @@ xcopy /seyi %1 %2
 xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\applicationContext-data.xml" "%2\WEB-INF\classes\applicationContext-data.xml"
 xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\datasources.properties" "%2\WEB-INF\classes\datasources.properties"
 xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\users.properties" "%2\WEB-INF\classes\users.properties"
+
+:eof
