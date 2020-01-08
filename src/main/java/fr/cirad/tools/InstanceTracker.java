4607f23fef16393744a2a5dbe37a5b1e89460f71
abbe <magsen.abbe@cirad.fr>
2020-01-08 15:42:40 +0100

    Tracker: database + locale

--- src/main/java/fr/cirad/tools/InstanceTracker.java
+++ src/main/java/fr/cirad/tools/InstanceTracker.java
@@ -3,6 +3,7 @@ package fr.cirad.tools;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Locale;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,28 +12,33 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import fr.cirad.security.ReloadableInMemoryDaoImpl;
+import fr.cirad.tools.mongo.MongoTemplateManager;
 
 @Configuration
 @EnableScheduling
 public class InstanceTracker {
-	private static final Logger LOG = Logger.getLogger(GigwaModuleManager.class);
+	private static final Logger LOG = Logger.getLogger(InstanceTracker.class);
 	
 	@Autowired private ReloadableInMemoryDaoImpl userDao;
 
 	/**
      * runs every 5 seconds ( for test purpose )
      */
-//	@Scheduled(fixedRate = 1000 * 5)
+	@Scheduled(fixedRate = 1000 * 5)
 	
     /**
      * runs every 7 days
      */
-	@Scheduled(fixedRate = 1000 * 60 * 60 * 24 * 7)
+//	@Scheduled(fixedRate = 1000 * 60 * 60 * 24 * 7)
 	public void track() {
-		int users = userDao.getUserMap().getUserCount();
-//		LOG.info("TRACKER users:" + users);
 		try {
-			URL url = new URL("http://localhost/test?instance=eclipse&country=FR&users=" + users + "&databases=1");
+			int users = userDao.getUserMap().getUserCount();
+//			LOG.info("TRACKER users:" + users);
+			long databases = MongoTemplateManager.getPublicDatabases().stream().count();
+//			LOG.info("TRACKER databases:" + databases);
+			String locale = Locale.getDefault().toString();
+//			LOG.info("TRACKER locale:" + locale);
+			URL url = new URL("http://localhost/instancetracker?instance=eclipse&country=" + locale + "&users=" + users + "&databases=" + databases);
 //			LOG.info(url);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
