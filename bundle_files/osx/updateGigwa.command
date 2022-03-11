daa1d6036ec24fb4d9ea7e16977a5dfeb98b06dc
Guilhem SEMPERE <guilhem.sempere@cirad.fr>
2022-03-11 09:39:51 +0100

    Updates switch to new security XML file without loosing passwordEncoder

--- bundle_files/osx/updateGigwa.command
+++ bundle_files/osx/updateGigwa.command
@@ -21,7 +21,7 @@ if [ -d "$1" ]; then
 
 		#copy configuration files from saved folder to new gigwa folder
 		cp -av $3/gigwa.$DATE/WEB-INF/classes/applicationContext-data.xml $2/WEB-INF/classes/applicationContext-data.xml
-		cp -av $3/gigwa.$DATE/WEB-INF/classes/applicationContext-security.xml $2/WEB-INF/classes/applicationContext-security.xml
+#		cp -av $3/gigwa.$DATE/WEB-INF/classes/applicationContext-security.xml $2/WEB-INF/classes/applicationContext-security.xml
 		cp -av $3/gigwa.$DATE/WEB-INF/classes/datasources.properties $2/WEB-INF/classes/datasources.properties
 		cp -av $3/gigwa.$DATE/WEB-INF/classes/users.properties $2/WEB-INF/classes/users.properties
 		cp -av $3/gigwa.$DATE/WEB-INF/classes/config.properties $2/WEB-INF/classes/config.properties
@@ -29,13 +29,19 @@ if [ -d "$1" ]; then
 
 		# Changes specific to migration to v2.5
 		sed -i 's/project\$CREATOR/SUPERVISOR/g' $2/WEB-INF/classes/users.properties	# replace the deprecated project-CREATOR role with the new DB-level SUPERVISOR role
+
 		if [ "$(grep -c dumpFolder $2/WEB-INF/classes/config.properties)" -eq 0 ]; then
 			printf "\ndumpFolder=$HOME/gigwaDumps\n" >> $2/WEB-INF/classes/config.properties	# add dumpFolder entry to config.properties it not present
 		fi		
+		
+		# applicationContext-security.xml has changed much in v2.5 so we don't want to keep that of the previous version. However, still make sure we keep the same passwordEncoder we had
+		if [ "$(grep 'id="passwordEncoder"' $3/gigwa.$DATE/WEB-INF/classes/applicationContext-security.xml | grep -v '<!--')" != "$(grep 'id="passwordEncoder"' $2/WEB-INF/classes/applicationContext-security.xml | grep -v '<!--')" ]; then
+			sed -i "s/.*PasswordEncoder.*//g;s/.*Only one passwordEncoder bean should be enabled at a time.*//g;s|</secur:http>|</secur:http>\n\n\n$(grep 'id="passwordEncoder"' $3/gigwa.$DATE/WEB-INF/classes/applicationContext-security.xml | grep -v '<!--')|g;" $2/WEB-INF/classes/applicationContext-security.xml
+		fi
 	fi
 fi
 
 chmod -R 755 $2
 
-echo
+echo -------------------------------
 echo "Update complete. You may want to apply chown -R on the updated folder."
--- bundle_files/windows/updateGigwa.bat
+++ bundle_files/windows/updateGigwa.bat
@@ -16,7 +16,8 @@ if not exist %2 (
 	echo %2 not exist
 	GOTO eof )
 
-set d=%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%
+set timeString=%time: =0%
+set d=%date:~6,4%%date:~3,2%%date:~0,2%%timeString:~0,2%%timeString:~3,2%
 
 ::save old gigwa files
 xcopy /seyi %2 "%3\gigwa.%d%"
@@ -29,19 +30,21 @@ xcopy /seyi %1 %2
 
 ::copy configuration files from saved folder to new gigwa folder
 xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\applicationContext-data.xml" "%2\WEB-INF\classes\applicationContext-data.xml"
-xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\applicationContext-security.xml" "%2\WEB-INF\classes\applicationContext-security.xml"
+::xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\applicationContext-security.xml" "%2\WEB-INF\classes\applicationContext-security.xml"
 xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\datasources.properties" "%2\WEB-INF\classes\datasources.properties"
 xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\users.properties" "%2\WEB-INF\classes\users.properties"
 xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\config.properties" "%2\WEB-INF\classes\config.properties"
 xcopy /seyi "%3\gigwa.%d%\WEB-INF\classes\log4j.xml" "%2\WEB-INF\classes\log4j.xml"
 
-# Changes specific to migration to v2.5
-powershell -Command "(gc %2\WEB-INF\classes\users.properties) -replace 'project$CREATOR', 'SUPERVISOR' | Out-File %2\WEB-INF\classes\users.properties"		# replace the deprecated project-CREATOR role with the new DB-level SUPERVISOR role
+:: Changes specific to migration to v2.5
+powershell -Command "(gc %2\WEB-INF\classes\users.properties) -replace 'project$CREATOR', 'SUPERVISOR' | Out-File -Encoding ascii %2\WEB-INF\classes\users.properties"		# replace the deprecated project-CREATOR role with the new DB-level SUPERVISOR role
+
 powershell -Command "if ($(Select-String -Path %2\WEB-INF\classes\config.properties -Pattern 'dumpFolder') -eq $null) {Add-Content %2\WEB-INF\classes\config.properties \"`ndumpFolder=%USERPROFILE%\gigwaDumps`n\".replace('\', '\\'")}"# add dumpFolder entry to config.properties it not present
 
-chmod -R 755 $2
+:: applicationContext-security.xml has changed much in v2.5 so we don't want to keep that of the previous version. However, still make sure we keep the same passwordEncoder as in the previous version
+powershell -Command "$previousPE = $(Select-String -Path %3\gigwa.%d%\WEB-INF\classes\applicationContext-security.xml -Pattern 'id=\"passwordEncoder\"' | Select-String -Pattern '<!--' -NotMatch | ForEach-Object {$_.Line}); if ($(Select-String -Path %2\WEB-INF\classes\applicationContext-security.xml -Pattern 'id=\"passwordEncoder\"' | Select-String -Pattern '<!--' -NotMatch | ForEach-Object {$_.Line}) -ne $previousPE) {(gc %1\WEB-INF\classes\applicationContext-security.xml) -creplace '.*PasswordEncoder.*', '' -creplace '.*Only one passwordEncoder bean should be enabled at a time.*', '' -creplace '.*</beans>.*', '' | Out-File -Encoding ascii %2\WEB-INF\classes\applicationContext-security.xml; Add-Content -Path %2\WEB-INF\classes\applicationContext-security.xml -value $previousPE; Add-Content -Path %2\WEB-INF\classes\applicationContext-security.xml -value '</beans>'} else {Write-Host 'same'}"
 
-echo
+echo -------------------------------
 echo Update complete.
 
 :eof
