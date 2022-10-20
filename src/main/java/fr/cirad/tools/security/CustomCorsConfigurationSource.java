8510c2460dcdba691467058424d29ad96c40b7a2
sempere <guilhem.sempere@cirad.fr>
2022-10-20 18:07:25 +0200

    Merge from master + POM update

--- src/main/java/fr/cirad/tools/security/CustomCorsConfigurationSource.java
+++ src/main/java/fr/cirad/tools/security/CustomCorsConfigurationSource.java
@@ -13,29 +13,30 @@ import fr.cirad.tools.AppConfig;
 
 public class CustomCorsConfigurationSource extends UrlBasedCorsConfigurationSource {
 
-    public static final String CORS_PROPERTY_PREFIX = "corsOrigins_";
+    public static final String ALLOWED_ORIGINS_PROPERTY_PREFIX = "allowedOrigins_";
 
-        public CustomCorsConfigurationSource(AppConfig appConfig) {
-            super();
-                
-            Map<String /*endpoint URL*/, Collection<String /*allowed origin*/>> customConfigs = new HashMap<>();
-            Map<String, String> corsConfigEntries = appConfig.getPrefixed(CORS_PROPERTY_PREFIX);
-            for (String key : corsConfigEntries.keySet())
-                if (key.startsWith(CORS_PROPERTY_PREFIX))
-                    customConfigs.put(key.substring(CORS_PROPERTY_PREFIX.length()).replaceFirst(".*/rest/", "/"), Arrays.stream(corsConfigEntries.get(key).split(",")).map(String :: trim).collect(Collectors.toList()));
+    public CustomCorsConfigurationSource(AppConfig appConfig) {
+        super();
             
-                for (String endpoint : customConfigs.keySet()) {
-                    CorsConfiguration configuration = new CorsConfiguration();
-                    for (String allowedOrigin : customConfigs.get(endpoint))
-                        configuration.addAllowedOrigin(allowedOrigin);
-                    configuration.setAllowedHeaders(Arrays.asList("*"));
-                    configuration.setAllowedMethods(Arrays.asList("*"));
-//	                configuration.addAllowedMethod("POST");
-//	                configuration.setAllowCredentials(false);
-//	                configuration.setMaxAge(Long.MAX_VALUE);
-                    registerCorsConfiguration(endpoint, configuration);
+        Map<String /*endpoint URL*/, Collection<String /*allowed origin*/>> customConfigs = new HashMap<>();
+        Map<String, String> corsConfigEntries = appConfig.getPrefixed(ALLOWED_ORIGINS_PROPERTY_PREFIX);
+        for (String key : corsConfigEntries.keySet())
+            if (key.startsWith(ALLOWED_ORIGINS_PROPERTY_PREFIX))
+                customConfigs.put(key.substring(ALLOWED_ORIGINS_PROPERTY_PREFIX.length()).replaceFirst(".*/rest/", "/"), Arrays.stream(corsConfigEntries.get(key).split(",")).map(String :: trim).collect(Collectors.toList()));
+        
+        for (String endpoint : customConfigs.keySet()) {
+            CorsConfiguration configuration = new CorsConfiguration();
+            for (String allowedOrigin : customConfigs.get(endpoint))
+                configuration.addAllowedOrigin(allowedOrigin);
 
-                    System.err.println(configuration);
-                }
+            configuration.setAllowedHeaders(Arrays.asList("*"));
+            configuration.setAllowedMethods(Arrays.asList("*"));
+//	        configuration.addAllowedMethod("POST");
+//	        configuration.setAllowCredentials(false);
+//	        configuration.setMaxAge(Long.MAX_VALUE);
+            registerCorsConfiguration(endpoint, configuration);
+
+//          System.err.println(configuration);
         }
+    }
 }
