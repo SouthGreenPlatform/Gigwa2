18db154e3fe67b56d90a6a0f11ee1787353e8367
GuilhemSempere <guilhem.sempere@cirad.fr>
2024-10-17 12:33:54 +0200

    Custom look (#156)
    * Provided means to customize the UI
    
    (Windows update script not tested yet)
    
    * Fixed Windows update script

--- bundle_files/ubuntu/updateGigwa.sh
+++ bundle_files/ubuntu/updateGigwa.sh
@@ -39,6 +39,8 @@ if [ -d "$1" ]; then
 		cp -avr $3/$backup_dir.$DATE/WEB-INF/classes/users.properties $2/WEB-INF/classes/users.properties
 		cp -avr $3/$backup_dir.$DATE/WEB-INF/classes/config.properties $2/WEB-INF/classes/config.properties
 		cp -avr $3/$backup_dir.$DATE/WEB-INF/classes/log4j.xml $2/WEB-INF/classes/log4j.xml
+		cp -avr $3/$backup_dir.$DATE/custom $2/custom
+
 
 		# Changes specific to migration to v2.5
 		sed -i 's/project\$CREATOR/SUPERVISOR/g' $2/WEB-INF/classes/users.properties	# replace the deprecated project-CREATOR role with the new DB-level SUPERVISOR role
@@ -54,7 +56,13 @@ if [ -d "$1" ]; then
 		# applicationContext-security.xml has changed much in v2.5 so we don't want to keep that of the previous version. However, still make sure we keep the same passwordEncoder we had
 		if [ "$(grep 'id="passwordEncoder"' $3/$backup_dir.$DATE/WEB-INF/classes/applicationContext-security.xml | grep -v '<!--')" != "$(grep 'id="passwordEncoder"' $2/WEB-INF/classes/applicationContext-security.xml | grep -v '<!--')" ]; then
 			sed -i "s/.*PasswordEncoder.*//g;s/.*Only one passwordEncoder bean should be enabled at a time.*//g;s|</secur:http>|</secur:http>\n\n\n$(grep 'id="passwordEncoder"' $3/$backup_dir.$DATE/WEB-INF/classes/applicationContext-security.xml | grep -v '<!--')|g;" $2/WEB-INF/classes/applicationContext-security.xml
-		fi	
+		fi
+
+
+		# Changes specific to migration to v2.9
+		if [ "$(grep '<context:component-scan base-package' $1/WEB-INF/classes/applicationContext-MVC.xml)" != "$(grep '<context:component-scan base-package' $2/WEB-INF/classes/applicationContext-MVC.xml)" ]; then
+			sed -i "s|<context:component-scan base-package.*|<context:component-scan base-package=\"fr.cirad.mgdb.service,fr.cirad.web.controller,fr.cirad.manager,fr.cirad.configuration,org.brapi.v2.api,fr.cirad.web.controller,fr.cirad.service\" />|g" $2/WEB-INF/classes/applicationContext-MVC.xml
+		fi
 	fi
 fi
 
